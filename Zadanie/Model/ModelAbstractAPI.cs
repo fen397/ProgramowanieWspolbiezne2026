17082eaf81ebc3d6703686cb05ed1776273f7b3b
Hubert Beinka <254716@edu.p.lodz.pl>
2026-04-17 13:56:33 +0200

    Dodanie abstrakcyjnej klasy ModelAbstractApi oraz implementacji ModelApi

---
+++ Zadanie/Model/ModelAbstractAPI.cs
@@ -0,0 +1,37 @@
+﻿namespace Model;
+using Logika;
+using Dane;
+
+public abstract class ModelAbstractApi
+{
+    public abstract void Start(int ballCount);
+    public abstract void Stop();
+    public abstract IEnumerable<Ball> GetBalls();
+
+    public static ModelAbstractApi CreateApi(LogicAbstractApi? logicApi = null)
+    {
+        return new ModelApi(logicApi ?? LogicAbstractApi.CreateApi());
+    }
+    
+    
+}
+
+internal class ModelApi : ModelAbstractApi
+{
+    private readonly LogicAbstractApi _logicApi;
+
+    public ModelApi(LogicAbstractApi logicApi)
+    {
+        _logicApi = logicApi;
+    }
+
+    public override void Start(int ballCount)
+    {
+        _logicApi.CreateBalls(ballCount);
+        _logicApi.Start();
+    }
+    
+    public override void Stop() => _logicApi.Stop();
+    public override IEnumerable<Ball> GetBalls() => _logicApi.GetBalls();
+}
+
