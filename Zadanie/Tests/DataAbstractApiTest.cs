0858ff1f7f2d3cdf1b2f75438391cd6d0ff9a517
Hubert Beinka <254716@edu.p.lodz.pl>
2026-04-19 12:45:39 +0200

    Napisanie testów jednostkowych do logiki i danych

---
+++ Zadanie/Tests/DataAbstractApiTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Dane;
+using JetBrains.Annotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests;
+
+[TestClass]
+[TestSubject(typeof(DataAbstractApi))]
+public class DataAbstractApiTest
+{
+
+    [TestMethod]
+    public void CreateBallsTest()
+    {
+        DataAbstractApi api = DataAbstractApi.CreateApi();
+        
+        api.CreateBalls(5);
+        var balls = api.GetBalls();
+        
+        Assert.AreEqual(5, ((List<Ball>)balls).Count);
+
+    }
+    
+    [TestMethod]
+    public void BoardDimensionTest()
+    {
+        DataAbstractApi api = DataAbstractApi.CreateApi();
+
+        var board = api.GetBoard();
+        
+        Assert.AreEqual(800, board.Width);
+        Assert.AreEqual(600, board.Height);
+
+    }
+}
---
+++ Zadanie/Tests/LogicAbstractApiTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using JetBrains.Annotations;
+using Logika;
+using Dane;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests;
+
+[TestClass]
+[TestSubject(typeof(LogicAbstractApi))]
+public class LogicAbstractApiTest
+{
+
+    [TestMethod]
+    public void BallsWithinBoardBoundariesTest()
+    {
+        LogicAbstractApi logicApi = LogicAbstractApi.CreateApi();
+        int ballCount = 10;
+        
+        logicApi.CreateBalls(ballCount);
+        var balls = logicApi.GetBalls().ToList();
+
+        foreach (var ball in balls )
+        {
+            Assert.IsTrue(ball.X >= 0);
+            Assert.IsTrue(ball.Y >= 0);
+            Assert.IsTrue(ball.X <= 800 - ball.Radius * 2);
+            Assert.IsTrue(ball.Y <= 800 - ball.Radius * 2);
+            
+        }
+    }
+}
